05b0e796e7c19db9b26fb6fafa6109d1a1b3fb47
shivani-pawade <122336176+shivani-pawade@users.noreply.github.com>
2025-11-25 17:30:49 +0530

    Update Consent.js

--- Consent.js
+++ Consent.js
@@ -1,18 +1,1 @@
-let cookieModel=document.querySelector(".cookie-consent-model")
-let cancelCookieBtn=document.querySelector(".btn.Cancel")
-let acceptCookieBtn=document.querySelector(".btn.Accept")
 
-cancelCookieBtn.addEventListener("click",function(){
-    cookieModel.classList.remove("active")
-})
-
-acceptCookieBtn.addEventListener("click",function(){
-    cookieModel.classList.remove("active")
-    localStorage.setItem("cookieAccepted", "yes")
-})
-setTimeout(function(){
-    let cookieAccepted=localStorage.getItem("cookieAccepted")
-    if(cookieAccepted != "yes"){
-    cookieModel.classList.add("active")
-    }
-},2000)
